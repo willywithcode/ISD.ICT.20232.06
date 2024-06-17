a1df06593b50f4c2f1c84e52a9c13ce927b7655c
unknown <vm.dung.learning@gmail.com>
2024-06-17 12:11:56 +0700

    Fix User roles

--- AIMS/src/controller/ManagerScreenController.java
+++ AIMS/src/controller/ManagerScreenController.java
@@ -6,14 +6,14 @@ import java.util.List;
 import entity.user.User;
 
 public class ManagerScreenController extends BaseController{
-    public void createUser(int id, String name, String email, String address, String phone, String role, String password) throws SQLException {
+    public void createUser(int id, String name, String email, String address, String phone, List<String> roles, String password) throws SQLException {
         User  user = new User();
-        user.createUser(id, name, email,address,  phone, role, password);
+        user.createUser(id, name, email,address,  phone, roles, password);
     }
     
-    public void updateUser(int id, String name, String email, String address, String phone, String role) throws SQLException {
+    public void updateUser(int id, String name, String email, String address, String phone, List<String> roles) throws SQLException {
         User  user = new User();
-        user.updateUser(id, name, email, address, phone, role);
+        user.updateUser(id, name, email, address, phone, roles);
     }
 
     public void deleteUser(int id) throws SQLException {
@@ -29,5 +29,9 @@ public class ManagerScreenController extends BaseController{
     public void changePassword(int id, String password) throws SQLException{
         User user = new User();
         user.changePassword(id, password);
+    }
+    
+    public void getCurrentUser(String username) {
+    	
     }
 }
--- AIMS/src/views/screen/manager/ManageUserScreenHandler.java
+++ AIMS/src/views/screen/manager/ManageUserScreenHandler.java
@@ -3,6 +3,7 @@ package views.screen.manager;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -17,8 +18,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
@@ -30,9 +33,9 @@ import utils.Utils;
 import utils.Utils.CHECK;
 import views.screen.BaseScreenHandler;
 
-public class ManageUserScreenHandler extends BaseScreenHandler implements Initializable{
-	
-	@FXML
+public class ManageUserScreenHandler extends BaseScreenHandler implements Initializable {
+
+    @FXML
     private Button userBtn;
     @FXML
     private AnchorPane userForm;
@@ -48,10 +51,10 @@ public class ManageUserScreenHandler extends BaseScreenHandler implements Initia
     private Label userLabelForm;
     @FXML
     private Button saveCreateUserBtn, saveUpdateUserBtn, banUserBtn, createUserBtn, updateUserBtn, deleteUserBtn, changeUserPasswordBtn;
-    @FXML	
+    @FXML
     private AnchorPane subUserForm;
     @FXML
-    private  ChoiceBox<String> roleChoice;
+    private ListView<String> roleListView;
     @FXML
     private AnchorPane changePasswordForm;
     @FXML
@@ -59,70 +62,90 @@ public class ManageUserScreenHandler extends BaseScreenHandler implements Initia
     @FXML
     private TextField newPasswordField, confirmPasswordField;
 
-	public ManageUserScreenHandler(Stage stage, String screenPath) throws IOException {
-		super(stage, screenPath);
-		// TODO Auto-generated constructor stub
-		userForm.setVisible(true);
-	}
-	
-	public ManagerScreenController getBController() {
+    private ObservableList<String> roles;
+    private ObservableList<String> selectedRoles;
+
+    public ManageUserScreenHandler(Stage stage, String screenPath) throws IOException {
+        super(stage, screenPath);
+        userForm.setVisible(true);
+    }
+
+    public ManagerScreenController getBController() {
         return (ManagerScreenController) super.getBController();
     }
 
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-		setBController(new ManagerScreenController());
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        setBController(new ManagerScreenController());
         try {
+            roles = FXCollections.observableArrayList("admin", "product manager");
+            selectedRoles = FXCollections.observableArrayList();
+            roleListView.setItems(roles);
+            roleListView.setCellFactory(param -> new ListCell<String>() {
+                private final CheckBox checkBox = new CheckBox();
+
+                @Override
+                protected void updateItem(String item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (item != null) {
+                        checkBox.setText(item);
+                        checkBox.setSelected(selectedRoles.contains(item));
+                        checkBox.selectedProperty().addListener((obs, wasSelected, isNowSelected) -> {
+                            if (isNowSelected) {
+                                selectedRoles.add(item);
+                            } else {
+                                selectedRoles.remove(item);
+                            }
+                        });
+                        setGraphic(checkBox);
+                    } else {
+                        setGraphic(null);
+                    }
+                }
+            });
             showAllUser();
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        }	
-	}
-	
-	public void showAllUser() throws SQLException{
-	    List<User> listUser = getBController().getAllUser();
-	    System.out.println(new PropertyValueFactory<User, String>("province"));
-	    roleChoice.getItems().setAll("Admin", "Product Manager");
-	    userIDCol.setCellValueFactory(new PropertyValueFactory<User, Integer>("id"));
-	    userUsernameCol.setCellValueFactory(new PropertyValueFactory<User, String>("username"));
-	    userEmailCol.setCellValueFactory(new PropertyValueFactory<User, String>("email"));
-	    userAddressCol.setCellValueFactory(new PropertyValueFactory<User, String>("address"));
-	    userPhoneNumberCol.setCellValueFactory(new PropertyValueFactory<User, String>("phone"));
-	    userRoleCol.setCellValueFactory(new PropertyValueFactory<User, String>("role"));;
-	    
-	    userTableView.getItems().setAll(listUser);
-	
-	    subUserForm.setVisible(false);
-	    changePasswordForm.setVisible(false);
-	
-	    userTableView.setRowFactory(tv -> new TableRow<User>() {
-	        @Override
-	        public void updateItem(User item, boolean empty) {
-	            super.updateItem(item, empty) ;
-	            String AdminStyle = "-fx-font-weight: bold";
-	            String BanUserStyle = "-fx-background-color: red";
-	            String style = "";
-	            if (item == null) {
-	                setStyle("");
-	            } else if (item.getBan()) {
-	                style = BanUserStyle;
-	            } else {
-	                style = "";
-	            };
-	
-	            if (item == null) {
-	                setStyle("");
-	            } else if ("admin".equals(item.getRole())) {
-	                style = style +  ";" +AdminStyle;
-	            }
-	
-	            setStyle(style);
-	        }
-	    });
-	}
-	
-	public void setCreateUserBtn() {
+        }
+    }
+
+    public void showAllUser() throws SQLException {
+        List<User> listUser = getBController().getAllUser();
+        userIDCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        userUsernameCol.setCellValueFactory(new PropertyValueFactory<>("username"));
+        userEmailCol.setCellValueFactory(new PropertyValueFactory<>("email"));
+        userAddressCol.setCellValueFactory(new PropertyValueFactory<>("address"));
+        userPhoneNumberCol.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        userRoleCol.setCellValueFactory(new PropertyValueFactory<>("roles")); // Update to reflect roles
+
+        userTableView.getItems().setAll(listUser);
+
+        subUserForm.setVisible(false);
+        changePasswordForm.setVisible(false);
+
+        userTableView.setRowFactory(tv -> new TableRow<User>() {
+            @Override
+            public void updateItem(User item, boolean empty) {
+                super.updateItem(item, empty);
+                String adminStyle = "-fx-font-weight: bold";
+                String banUserStyle = "-fx-background-color: red";
+                String style = "";
+                if (item == null) {
+                    setStyle("");
+                } else if (item.getBan()) {
+                    style = banUserStyle;
+                } else {
+                    style = "";
+                }
+                if (item != null && item.getRoles().contains("admin")) {
+                    style += ";" + adminStyle;
+                }
+                setStyle(style);
+            }
+        });
+    }
+
+    public void setCreateUserBtn() {
         userLabelForm.setText("Create User");
         subUserForm.setVisible(true);
         saveUpdateUserBtn.setVisible(false);
@@ -133,13 +156,13 @@ public class ManageUserScreenHandler extends BaseScreenHandler implements Initia
         userAddressField.setText("");
         userPhoneNumberField.setText("");
         userEmailField.setText("");
+        selectedRoles.clear();
     }
 
     public void setUpdateUserBtn() {
         changePasswordForm.setVisible(false);
         User selectedUser = userTableView.getSelectionModel().getSelectedItem();
-        System.out.println(selectedUser);
-        if (selectedUser != null) {        	
+        if (selectedUser != null) {
             userLabelForm.setText("Edit User");
             subUserForm.setVisible(true);
             saveUpdateUserBtn.setVisible(true);
@@ -149,8 +172,31 @@ public class ManageUserScreenHandler extends BaseScreenHandler implements Initia
             userAddressField.setText(selectedUser.getAddress());
             userPhoneNumberField.setText(selectedUser.getPhone());
             userEmailField.setText(selectedUser.getEmail());
-            String role = selectedUser.getRole().toLowerCase();
-            roleChoice.setValue(role.substring(0, 1).toUpperCase() + role.substring(1));
+            selectedRoles.setAll(selectedUser.getRoles());
+            
+         // Update the checkboxes in the roleListView
+            roleListView.setCellFactory(param -> new ListCell<String>() {
+                private final CheckBox checkBox = new CheckBox();
+
+                @Override
+                protected void updateItem(String item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (item != null) {
+                        checkBox.setText(item);
+                        checkBox.setSelected(selectedRoles.contains(item));
+                        checkBox.selectedProperty().addListener((obs, wasSelected, isNowSelected) -> {
+                            if (isNowSelected) {
+                                selectedRoles.add(item);
+                            } else {
+                                selectedRoles.remove(item);
+                            }
+                        });
+                        setGraphic(checkBox);
+                    } else {
+                        setGraphic(null);
+                    }
+                }
+            });
         } else {
             subUserForm.setVisible(false);
         }
@@ -161,8 +207,7 @@ public class ManageUserScreenHandler extends BaseScreenHandler implements Initia
         changePasswordForm.setVisible(false);
         User selectedUser = userTableView.getSelectionModel().getSelectedItem();
         if (selectedUser != null) {
-            Alert confirmAlert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to ban / unban this user?",
-                    ButtonType.YES, ButtonType.NO);
+            Alert confirmAlert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to ban / unban this user?", ButtonType.YES, ButtonType.NO);
             Optional<ButtonType> result = confirmAlert.showAndWait();
             if (result.get() == ButtonType.YES) {
                 getBController().banUser(selectedUser.getId(), selectedUser.getBan());
@@ -176,14 +221,13 @@ public class ManageUserScreenHandler extends BaseScreenHandler implements Initia
         changePasswordForm.setVisible(false);
         User selectedUser = userTableView.getSelectionModel().getSelectedItem();
         if (selectedUser != null) {
-            Alert confirmAlert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to delete this user?",
-                    ButtonType.YES, ButtonType.NO);
+            Alert confirmAlert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to delete this user?", ButtonType.YES, ButtonType.NO);
             Optional<ButtonType> result = confirmAlert.showAndWait();
             if (result.get() == ButtonType.YES) {
                 getBController().deleteUser(selectedUser.getId());
                 showAllUser();
             }
-                    }
+        }
     }
 
     public void setChangeUserPasswordBtn() throws SQLException {
@@ -196,27 +240,27 @@ public class ManageUserScreenHandler extends BaseScreenHandler implements Initia
         String address = userAddressField.getText();
         String phone = userPhoneNumberField.getText();
         String email = userEmailField.getText();
-        String role_str = roleChoice.getSelectionModel().getSelectedItem().toString().toLowerCase();
-        String default_password = "123123";
+        List<String> roles = new ArrayList<>(selectedRoles);
+        String defaultPassword = "123123";
         int numberOfUser = getBController().getAllUser().size();
         int id = numberOfUser + 1;
 
-        CHECK check_phone = Utils.checkPhoneNumber(phone);
-        CHECK check_email = Utils.checkEmail(email);
+        CHECK checkPhone = Utils.checkPhoneNumber(phone);
+        CHECK checkEmail = Utils.checkEmail(email);
 
-        if (check_phone == CHECK.WRONG_PHONENUMBER || check_email == CHECK.WRONG_EMAIL) {
+        if (checkPhone == CHECK.WRONG_PHONENUMBER || checkEmail == CHECK.WRONG_EMAIL) {
             showAlert(Alert.AlertType.WARNING, "Fail to create new user", "Enter information again please", "Enter information again please");
         } else {
-        	if(Utils.usernameExists(username)) {
-        		showAlert(Alert.AlertType.WARNING, "Fail to create new user", "Username already exists", "Please choose a different username");
-        	}else {
-        		getBController().createUser(id, username, email, address, phone, role_str, default_password);
-        		showAllUser();	
-        	}
+            if (Utils.usernameExists(username)) {
+                showAlert(Alert.AlertType.WARNING, "Fail to create new user", "Username already exists", "Please choose a different username");
+            } else {
+                getBController().createUser(id, username, email, address, phone, roles, defaultPassword);
+                showAllUser();
+            }
         }
     }
 
-    public void setSaveUpdateUserBtn() throws SQLException{
+    public void setSaveUpdateUserBtn() throws SQLException {
         User selectedUser = userTableView.getSelectionModel().getSelectedItem();
         if (selectedUser != null) {
             int id = selectedUser.getId();
@@ -224,15 +268,15 @@ public class ManageUserScreenHandler extends BaseScreenHandler implements Initia
             String address = userAddressField.getText();
             String phone = userPhoneNumberField.getText();
             String email = userEmailField.getText();
-            String role_str = roleChoice.getSelectionModel().getSelectedItem().toString().toLowerCase();
-            
-            CHECK check_phone = Utils.checkPhoneNumber(phone);
-            CHECK check_email = Utils.checkEmail(email);
+            List<String> roles = new ArrayList<>(selectedRoles);
 
-            if (check_phone == CHECK.WRONG_PHONENUMBER || check_email == CHECK.WRONG_EMAIL) {
+            CHECK checkPhone = Utils.checkPhoneNumber(phone);
+            CHECK checkEmail = Utils.checkEmail(email);
+
+            if (checkPhone == CHECK.WRONG_PHONENUMBER || checkEmail == CHECK.WRONG_EMAIL) {
                 showAlert(Alert.AlertType.WARNING, "Fail to change user information", "Enter again please", "Enter again please");
             } else {
-                getBController().updateUser(id, name, email, address, phone, role_str);
+                getBController().updateUser(id, name, email, address, phone, roles);
                 showAllUser();
             }
         } else {
@@ -240,19 +284,17 @@ public class ManageUserScreenHandler extends BaseScreenHandler implements Initia
         }
     }
 
-    public void setSaveChangePassword() throws SQLException, Exception{
+    public void setSaveChangePassword() throws SQLException, Exception {
         User selectedUser = userTableView.getSelectionModel().getSelectedItem();
-        int id = selectedUser.getId();
         if (selectedUser != null) {
-            Alert confirmAlert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to change password?",
-                    ButtonType.YES, ButtonType.NO);
+            int id = selectedUser.getId();
+            Alert confirmAlert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to change password?", ButtonType.YES, ButtonType.NO);
             Optional<ButtonType> result = confirmAlert.showAndWait();
             if (result.get() == ButtonType.YES) {
                 String newPassword = newPasswordField.getText();
-                String confirmPassword =  confirmPasswordField.getText();
-                    if (newPassword .compareTo(confirmPassword)  != 0) {
-                    showAlert(Alert.AlertType.ERROR, "Wrong confirm password " + newPassword + confirmPassword, "The new password is not the same with the confirm password",
-                            "Please enter confirm password again.");
+                String confirmPassword = confirmPasswordField.getText();
+                if (!newPassword.equals(confirmPassword)) {
+                    showAlert(Alert.AlertType.ERROR, "Wrong confirm password", "The new password is not the same with the confirm password", "Please enter confirm password again.");
                 } else {
                     getBController().changePassword(id, newPassword);
                     changePasswordForm.setVisible(false);
@@ -260,7 +302,6 @@ public class ManageUserScreenHandler extends BaseScreenHandler implements Initia
             }
         }
     }
-
 
     private void showAlert(Alert.AlertType alertType, String title, String header, String content) {
         Alert alert = new Alert(alertType);
